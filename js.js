c6a5c65cd96789850f3727744f6191799b1db4ef
SKISTE <skiste4u@gmail.com>
2024-05-30 00:52:08 +0400

    Check

--- js.js
+++ js.js
@@ -3,26 +3,33 @@ const left = document.getElementsByClassName("left")[0];
 left.classList.add("disabled");
 const right = document.getElementsByClassName("right")[0];
 
+var startTime, endTime;
+function start_timer() {
+  console.log('start count elapse time')
+  startTime = performance.now();
+};
+
+function end_timer() {
+  endTime = performance.now();
+  var timeDiff = endTime - startTime; //in ms
+  console.log(timeDiff + " elapsed time");
+}
+
 const imagesUrl = [
-    "images/ (1).jpg",
-    "images/ (2).jpg",
-    "images/ (3).jpg",
-    "images/ (4).jpg",
-    "images/ (5).jpg",
-    "images/ (6).jpg",
-    "images/ (7).jpg",
-    "images/ (8).jpg",
-    "images/ (9).jpg",
-    "images/ (10).jpg",
-    "images/ (11).jpg",
-    "images/ (12).jpg",
-    "images/ (13).jpg",
-    "images/ (14).jpg",
-    "images/ (15).jpg",
-    "images/ (16).jpg",
-    "images/ (17).jpg",
-    "images/ (18).jpg",
+  'https://avatars.dzeninfra.ru/get-zen_doc/271828/pub_655dd52aeece0b744ad0048e_655dfa4311aa97744330de27/scale_1200',
+  'https://img.freepik.com/free-photo/fresh-yellow-daisy-single-flower-close-up-beauty-generated-by-ai_188544-15543.jpg?size=626&ext=jpg&ga=GA1.1.44546679.1716681600&semt=ais_user',
+  'https://img2.akspic.ru/previews/2/7/7/4/7/174772/174772-skelet-18650-past-rebro-kost-500x.jpg',
+  'https://png.pngtree.com/thumb_back/fw800/background/20230610/pngtree-picture-of-a-blue-bird-on-a-black-background-image_2937385.jpg',
+  'https://static.insales-cdn.com/images/products/1/2336/567986464/WhatsApp_Image_2022-07-13_at_23.31.04__3_.jpeg_-_%D0%A1%D1%80%D0%B5%D0%B4%D1%81%D1%82%D0%B2%D0%BE_%D0%BF%D1%80%D0%BE%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%B0_%D1%84%D0%BE%D1%82%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D0%B9_Wiows.png',
+  'https://w7.pngwing.com/pngs/835/230/png-transparent-desktop-marble-wall-decal-marble-miscellaneous-texture-branch-thumbnail.png',
+  'https://cs6.pikabu.ru/post_img/big/2017/06/30/4/1498801619124951735.jpg',
+  'https://masterpiecer-images.s3.yandex.net/34e974e177bb11ee9ad5ceda526c50ab:upscaled'
 ];
+
+// for (let x = 0; x < 48; x++) {
+//   let elem = x+1
+//   imagesUrl.push('images/ ('+elem+').jpg')
+// }
 
 const images = imagesUrl.length;
 
@@ -49,19 +56,23 @@ function init() {
 init();
 
 right.onclick = function () {
+  start_timer()
   selected++;
   if (selected > images - 1) {
     selected = images - 1;
   }
   handleSelection();
+  end_timer()
 };
 
 left.onclick = function () {
+  start_timer()
   selected--;
   if (selected < 0) {
     selected = 0;
   }
   handleSelection();
+  end_timer()
 };
 
 function handleSelection() {
